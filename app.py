3b4766be597d587443f7a0873ca4ad3521667346
Nischal Thapaliya <nischalthapaliya@Nischals-MacBook-Air.local>
2024-09-04 23:48:47 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
@@ -6,6 +6,9 @@ from tensorflow.keras.models import load_model
 import numpy as np
 
 app = Flask(__name__)
+
+# Use the existing "uploads" directory inside the "static" folder
+UPLOAD_FOLDER = 'static/uploads'
 
 # Load your trained model
 MODEL_PATH = 'models/model_densenet.h5'
@@ -16,7 +19,8 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file:
-            filepath = os.path.join('uploads', file.filename)
+            # Save the uploaded file to the existing static/uploads directory
+            filepath = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(filepath)
             
             # Predict
@@ -27,8 +31,10 @@ def upload_file():
             predicted_class = np.argmax(pred, axis=1)
             label = 'Benign' if predicted_class[0] == 0 else 'Malignant'
             
-            return label
+            # Pass the relative file path and result to the template
+            return render_template('upload.html', result=label, filepath=filepath)
     return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
+
